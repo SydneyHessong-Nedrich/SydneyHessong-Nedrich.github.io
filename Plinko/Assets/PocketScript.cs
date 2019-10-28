6cf292c99900d738d7dac116dfb103f95c396397
SydneyHessong-Nedrich <55705797+SydneyHessong-Nedrich@users.noreply.github.com>
2019-10-28 10:16:21 -0500

    Plinko upload

---
+++ Plinko/Assets/PocketScript.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PocketScript : MonoBehaviour
+{
+    public int scoreValue;
+    public DropperScript dropper;
+    private void OnTriggerEnter(Collider other)
+    {
+        //give dropperScript the points to add to score
+        dropper.AddScore(scoreValue);
+
+        Destroy(other.gameObject);
+    }
+}
